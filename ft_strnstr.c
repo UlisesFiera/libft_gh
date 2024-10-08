f620821f655b1b943dcbf07eb1b076c607e97b07
Ulises Fernandez Del Campo Jorquera <ulfernan@c2r11s1.42madrid.com>
2024-10-08 16:31:45 +0200

    asdasd

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ulfernan <ulfernan@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 19:52:10 by ulfernan          #+#    #+#             */
-/*   Updated: 2024/10/08 16:26:49 by ulfernan         ###   ########.fr       */
+/*   Updated: 2024/10/08 16:31:21 by ulfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
+	i = 0;
 	if (!haystack || !needle)
 		return (NULL);
 	if (*needle == '\0')
 		return ((char *)haystack);
-	i = 0;
 	while (i < len && haystack[i] != '\0')
 	{
 		j = 0;
 		while (i + j < len && haystack[i + j] == needle[j])
 		{
+			j++;
 			if (needle[j] == '\0')
 				return ((char *)&haystack[i]);
-			j++;
 		}
 		i++;
 	}
